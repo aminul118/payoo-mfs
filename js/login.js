de1d79bb3988f4ab17f0b32f8c41cf79accf8fd3
Md Aminul Islam <mr.aminul118@gmail.com>
2024-09-18 19:31:45 +0600

    Home Page added

--- js/login.js
+++ js/login.js
@@ -12,8 +12,17 @@ document.getElementById('button-login').addEventListener('click', function (even
     event.preventDefault();  //! Browser এর ডিফল্ট রিলোডিং অফ করে
     console.log('log in button clicked');
     // step 3: Get the phone number
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber.value);
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber, phoneNumber);
+
+    // !This is not for ultimate / perfect way 
+    if (phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are logged in');
+    }
+    else{
+        alert('Wrong phone Number or Password')
+    }
 
 
 })
