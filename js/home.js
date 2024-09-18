ac00b95d653c5a2ff5ed5fb230971f188ed5cafe
Md Aminul Islam <mr.aminul118@gmail.com>
2024-09-18 20:50:36 +0600

    Partially added add money system

--- js/home.js
+++ js/home.js
@@ -1,1 +1,19 @@
 // console.log('home added');
+/***
+ * * S1: add event handler
+ * Prevent page reload after form submit
+ * * s2: add an event handler to the add money button inside the form
+ */
+
+// S1.
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+    // S2:  Money added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+
+
+    // Get the pin number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(addMoneyInput, pinNumberInput);
+
+})
